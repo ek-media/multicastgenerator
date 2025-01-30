87f5412d2154b657809dd103fa6baf3b20b2a944
Sebastiaan Eekhof <sebastiaan@eekhof.dev>
2025-01-30 12:42:07 +0100

    fix

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-git pull
-yarn
-yarn build
-yarn prod:restart
+BRANCH="main"
+git remote update
+LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
+LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
+
+if [ $LAST_COMMIT != $LAST_UPDATE ]; then
+    echo "Updating application"
+    git pull --no-edit
+    yarn
+    yarn build
+    yarn prod:restart
+else
+    echo "No updates available"
+fi
